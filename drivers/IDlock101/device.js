e4ad3790df67386b419e2d99588be778e9396871
dT <dTNL.Homey@gmail.com>
2017-10-07 00:10:37 +0200

    Add debugging

--- drivers/IDlock101/device.js
+++ drivers/IDlock101/device.js
@@ -6,10 +6,16 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class IDLock101 extends ZwaveDevice {
 	onMeshInit() {
+		// enable debugging
+		this.enableDebug();
+
+		// print the node's info to the console
+		this.printNode();
+
 		this.registerCapability('locked', 'DOOR_LOCK');
 		this.registerCapability('locked', 'NOTIFICATION');
-		this.registerCapability('measure_battery', 'BATTERY');
-		this.registerCapability('alarm_battery', 'BATTERY');
+		// this.registerCapability('measure_battery', 'BATTERY');
+		// this.registerCapability('alarm_battery', 'BATTERY');
 	}
 }
 module.exports = IDLock101;
