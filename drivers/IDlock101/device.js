2fb6fd8004e80c7c10f773b21da9e093742acfa7
dT <dTNL.Homey@gmail.com>
2017-10-12 20:28:00 +0200

    change to ZwaveLockDevice

--- drivers/IDlock101/device.js
+++ drivers/IDlock101/device.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+const ZwaveLockDevice = require('homey-meshdriver').ZwaveLockDevice;
 
 // Documentation: https://Products.Z-WaveAlliance.org/ProductManual/File?folder=&filename=Manuals/2293/IDL Operational Manual EN v1.3.pdf
 
-class IDLock101 extends ZwaveDevice {
+class IDLock101 extends ZwaveLockDevice {
 	onMeshInit() {
 		// enable debugging
 		this.enableDebug();
@@ -12,16 +12,6 @@ class IDLock101 extends ZwaveDevice {
 		// print the node's info to the console
 		this.printNode();
 
-		this.registerCapability('locked', 'DOOR_LOCK', {
-			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				pollInterval: 'poll_interval_battery' // maps to device settings
-				// getOnWakeUp: true, // only useful for battery devices
-			}
-		});
-		// this.registerCapability('locked', 'NOTIFICATION');
-		this.registerCapability('measure_battery', 'BATTERY');
-		this.registerCapability('alarm_battery', 'BATTERY');
 	}
 }
 module.exports = IDLock101;
