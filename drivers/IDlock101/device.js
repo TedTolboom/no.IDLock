98d6b9752445b1f3f865aa3d0c2b8e16c8ab2835
dT <dTNL.Homey@gmail.com>
2017-11-28 00:11:49 +0100

    initial commit

--- drivers/IDlock101/device.js
+++ drivers/IDlock101/device.js
@@ -14,13 +14,51 @@ class IDLock101 extends ZwaveDevice {
 
 		this.registerCapability('locked', 'DOOR_LOCK', {
 			getOpts: {
-				getOnStart: true, // get the initial value on app start
-				// pollInterval: 'poll_interval_battery' // maps to device settings
-				// getOnWakeUp: true, // only useful for battery devices
+				getOnStart: true,
 			}
+		});
+		this.registerCapability('door', 'DOOR_LOCK', {
+			get: 'DOOR_LOCK_OPERATION_GET',
+			getOpts: {
+				getOnStart: true,
+			},
+			report: 'DOOR_LOCK_OPERATION_REPORT',
+			reportParserV2(report) {
+				if (report && report.hasOwnProperty('Door Condition')) {
+					var doorConditionBinaryArray = (report['Door Condition']).toString(2);
+					this.log('Door Condition has changed:', report['Door Condition'], 'binary array', doorConditionBinaryArray)
+					return (doorConditionBinaryArray[0] === 1) ? 0 : 1;
+				};
+				return null;
+			},
 		});
 		// this.registerCapability('locked', 'NOTIFICATION');
 		this.registerCapability('measure_battery', 'BATTERY');
+		this.registerCapability('alarm_tamper', 'NOTIFICATION');
+		this.registerCapability('alarm_fire', 'NOTIFICATION', {
+			get: 'NOTIFICATION_GET',
+			getOpts: {
+				getOnStart: true,
+			},
+			getParser: () => ({
+				'V1 Alarm Type': 0,
+				'Notification Type': 'Emergency',
+				Event: 2,
+			}),
+			report: 'NOTIFICATION_REPORT',
+			reportParser: report => {
+				if (report && report['Notification Type'] === 'Emergency' && report.hasOwnProperty('Event (Parsed)')) {
+					if (report['Event (Parsed)'] === 'Contact Fire Service') return true;
+					if (report['Event (Parsed)'] === 'Event inactive' &&
+						report.hasOwnProperty('Event Parameter') &&
+						(report['Event Parameter'][0] === 2 ||
+							report['Event Parameter'][0] === 254)) {
+						return false;
+					}
+				}
+				return null;
+			}
+		});
 		this.registerCapability('alarm_battery', 'BATTERY');
 
 		this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', (rawReport, parsedReport) => {
