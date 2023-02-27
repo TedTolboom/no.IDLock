e25fc85d1ef3a8627d169b1eefbecd845643852c
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2023-02-27 16:00:38 +0100

    onMeshInit is deprecated infavour for onNodeInit in homey-zwavedriver

--- drivers/IDlock101/device.js
+++ drivers/IDlock101/device.js
@@ -5,13 +5,14 @@ const { ZwaveDevice } = require('homey-zwavedriver');
 // Documentation: https://Products.Z-WaveAlliance.org/ProductManual/File?folder=&filename=Manuals/2293/IDL Operational Manual EN v1.3.pdf
 
 class IDlock101 extends ZwaveDevice {
-	onMeshInit() {
+
+	async onNodeInit() {
+
 		// enable debugging
 		// this.enableDebug();
 
 		// print the node's info to the console
 		// this.printNode();
-
 		
 		this.registerCapability('locked', 'DOOR_LOCK', {
 			getOpts: {
